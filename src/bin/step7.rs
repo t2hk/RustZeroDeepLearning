01ec118c58eb305bd4ebf612a4ce49348f7b1718
t2hk <30774256+t2hk@users.noreply.github.com>
2025-02-22 14:45:05 +0000

    ステップ7の実装検討

--- src/bin/step7.rs
+++ src/bin/step7.rs
@@ -1,32 +1,26 @@
-//! ステップ7 バックプロパゲーションの自動化
-//!
-//! 実行方法
-//! ```
-//! cargo run --bin [RSファイル]
-//! ```
-
 use ndarray::{Array, IxDyn};
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
 
 /// Variable 構造体
 /// * data (Array<f64, IxDyn>): 変数
 /// * grad (Option<Array<f64, IxDyn>): 変数に対応した微分した値。逆伝播によって実際に微分が計算されたときに値を設定する。
-/// * creator (Option<>)
-struct Variable<'a, F> {
+/// * creator (Option<Weak<RefCell<dyn Function>>>): 生成元の関数
+#[derive(Debug, Clone)]
+struct Variable {
     data: Array<f64, IxDyn>,
     grad: Option<Array<f64, IxDyn>>,
-    creator: Option<&'a F>,
+    creator: Option<Weak<RefCell<dyn Function>>>,
 }
 
-impl<'a, F> Variable<'a, F> {
+impl Variable {
     /// Variable のコンストラクタ。
     ///
     /// # Arguments
     /// * data - 変数
-    /// * grad - data を微分した値
-    /// * creator - 生成元の関数
-    fn new(data: Array<f64, IxDyn>) -> Variable<'a, F> {
+    fn new(data: Array<f64, IxDyn>) -> Variable {
         Variable {
-            data: data,
+            data,
             grad: None,
             creator: None,
         }
@@ -36,14 +30,39 @@ impl<'a, F> Variable<'a, F> {
     ///
     /// # Arguments
     /// * creator - 生成元の関数
-    fn set_creator(&mut self, creator: &'a F) {
-        self.creator = Some(creator);
+    fn set_creator(&mut self, creator: Rc<RefCell<dyn Function>>) {
+        self.creator = Some(Rc::downgrade(&creator));
+    }
+
+    fn backward(self) {
+        match self.creator {
+            Some(creator) => {
+                let x = creator.upgrade().unwrap().borrow().get_input();
+                println!("valiable backword x: {:?}", x);
+                // match x {
+                //     Some(x) => {
+                //         &x.grad = Some(
+                //             creator
+                //                 .upgrade()
+                //                 .unwrap()
+                //                 .borrow()
+                //                 .backward(self.grad.as_ref().unwrap()),
+                //         );
+                //         x.backward();
+                //     }
+                //     None => {}
+                // }
+            }
+            None => {
+                println!("variable backward: None");
+            }
+        }
     }
 }
 
 /// Function トレイト
 /// Variable を入力し、処理を実行して結果を Variable で返却する。
-trait Function<'a> {
+trait Function {
     /// 関数を実行する。
     /// 関数の実装は Function を継承して行う。
     ///
@@ -52,123 +71,240 @@ trait Function<'a> {
     ///
     /// # Return
     /// * Variable - 処理結果
-    fn call(&mut self, input: &'a Variable<'a, Self>) -> Variable<'a, Self>
-    where
-        Self: Sized,
-    {
+    fn call(&self, input: &Variable, self_rc: Rc<RefCell<dyn Function>>) -> Variable {
         let x = &input.data;
         let y = self.forward(x);
 
         let mut output = Variable::new(y);
-        self.keep_input(input);
-        output.set_creator(self); // 出力変数に生成元の関数を設定する。
+        output.set_creator(Rc::clone(&self_rc));
 
-        self.keep_output(&output);
+        self_rc
+            .borrow_mut()
+            .keep_input_output(input.clone(), output.clone());
 
         output
     }
 
-    fn keep_input(&mut self, input: &'a Variable<'a, Self>);
-    fn keep_output(&mut self, output: &'a Variable<'a, Self>);
+    fn keep_input_output(&mut self, input: Variable, output: Variable);
 
     /// 通常の計算を行う順伝播。継承して実装すること。
     fn forward(&self, x: &Array<f64, IxDyn>) -> Array<f64, IxDyn>;
 
     /// 微分の計算を行う逆伝播。継承して実装すること。
     fn backward(&self, gy: &Array<f64, IxDyn>) -> Array<f64, IxDyn>;
+
+    fn get_input(&self) -> Option<Variable>;
 }
 
 /// 二乗する。
-struct Square<'a> {
-    input: Option<&'a Variable<'a, Square<'a>>>,
-    output: Option<&'a Variable<'a, Square<'a>>>,
+#[derive(Debug, Clone)]
+struct Square {
+    input: Option<Weak<RefCell<Variable>>>,
+    output: Option<Weak<RefCell<Variable>>>,
 }
-impl<'a> Function<'a> for Square<'a> {
+
+impl Function for Square {
     /// 順伝播
     fn forward(&self, x: &Array<f64, IxDyn>) -> Array<f64, IxDyn> {
         x.mapv(|x| x.powi(2))
     }
 
-    fn keep_input(&mut self, input: &'a Variable<'a, Square<'a>>) {
-        self.input = Some(input);
+    fn keep_input_output(&mut self, input: Variable, output: Variable) {
+        self.input = Some(Rc::downgrade(&Rc::new(RefCell::new(input))));
+        self.output = Some(Rc::downgrade(&Rc::new(RefCell::new(output))));
     }
 
-    fn keep_output(&mut self, output: &'a Variable<'a, Square<'a>>) {
-        self.output = Some(output);
+    fn get_input(&self) -> Option<Variable> {
+        match &self.input {
+            Some(input) => {
+                let input = input.upgrade().unwrap().borrow().clone();
+                Some(input)
+            }
+            None => None,
+        }
     }
 
     /// 逆伝播
     /// y=x^2 の微分であるため、dy/dx=2x である。
     fn backward(&self, gy: &Array<f64, IxDyn>) -> Array<f64, IxDyn> {
-        let x = &self.input.unwrap().data;
+        let x = self
+            .input
+            .as_ref()
+            .unwrap()
+            .upgrade()
+            .unwrap()
+            .borrow()
+            .data
+            .clone();
         let gx = 2.0 * x * gy;
         gx
     }
 }
 
 /// Exp 関数
-struct Exp<'a> {
-    input: Option<&'a Variable<'a, Exp<'a>>>,
-    output: Option<&'a Variable<'a, Exp<'a>>>,
+#[derive(Clone)]
+struct Exp {
+    input: Option<Weak<RefCell<Variable>>>,
+    output: Option<Weak<RefCell<Variable>>>,
 }
-impl<'a> Function<'a> for Exp<'a> {
+
+impl Function for Exp {
     // Exp (y=e^x) の順伝播
     fn forward(&self, x: &Array<f64, IxDyn>) -> Array<f64, IxDyn> {
         let e = std::f64::consts::E;
         x.mapv(|x| e.powf(x))
     }
 
-    fn keep_input(&mut self, input: &'a Variable<'a, Exp<'a>>) {
-        self.input = Some(input);
+    fn keep_input_output(&mut self, input: Variable, output: Variable) {
+        self.input = Some(Rc::downgrade(&Rc::new(RefCell::new(input))));
+        self.output = Some(Rc::downgrade(&Rc::new(RefCell::new(output))));
     }
 
-    fn keep_output(&mut self, output: &'a Variable<'a, Exp<'a>>) {
-        self.output = Some(output);
+    fn get_input(&self) -> Option<Variable> {
+        match &self.input {
+            Some(input) => {
+                let input = input.upgrade().unwrap().borrow().clone();
+                Some(input)
+            }
+            None => None,
+        }
     }
-
     /// 逆伝播
     /// dy/dx=e^x である。
     fn backward(&self, gy: &Array<f64, IxDyn>) -> Array<f64, IxDyn> {
+        let x = &self
+            .input
+            .as_ref()
+            .unwrap()
+            .upgrade()
+            .unwrap()
+            .borrow()
+            .data
+            .clone();
         let e = std::f64::consts::E;
-        let x = &self.input.unwrap().data;
         let gx = x.mapv(|x| e.powf(x)) * gy;
         gx
     }
 }
 
 fn main() {
-    todo!();
-    // // 順伝播
-    // // x -> Square -> a -> Exp -> b -> Square -> y
-    // let mut a_square = Square { input: None };
-    // let mut b_exp = Exp { input: None };
-    // let mut c_square = Square { input: None };
+    // 順伝播
+    // x -> Square -> a -> Exp -> b -> Square -> y
+    let a_square = Square {
+        input: None,
+        output: None,
+    };
+    let b_exp = Exp {
+        input: None,
+        output: None,
+    };
+    let c_square = Square {
+        input: None,
+        output: None,
+    };
 
-    // let mut x = Variable::new(Array::from_elem(IxDyn(&[]), 0.5));
-    // println!("x: {}", x.data);
-    // let mut a = a_square.call(&x);
-    // println!("a: {}", a.data);
-    // let mut b = b_exp.call(&a);
-    // println!("b: {}", b.data);
-    // let mut y = c_square.call(&b);
-    // println!("y: {}", y.data);
+    let mut x = Variable::new(Array::from_elem(IxDyn(&[]), 0.5));
+    println!("x: {}", x.data);
+    let rc_a_square = Rc::new(RefCell::new(a_square.clone()));
+    let mut a = a_square.call(&x, rc_a_square);
+    println!("a: {}", a.data);
+    let rc_b_exp = Rc::new(RefCell::new(b_exp.clone()));
+    let mut b = b_exp.call(&a, rc_b_exp);
+    println!("b: {}", b.data);
+    let rc_c_square = Rc::new(RefCell::new(c_square.clone()));
+    let mut y = c_square.call(&b, rc_c_square);
+    println!("y: {}", y.data);
 
-    // // 逆伝播
-    // //x.grad <- a.backword <- a.grad <- b.backward <- b.grad <- c.backward <- y.grad(=1.0)
-    // y.grad = Some(Array::from_elem(IxDyn(&[]), 1.0));
-    // b.grad = Some(c_square.backward(&y.grad.unwrap()));
-    // a.grad = Some(b_exp.backward(&b.grad.unwrap()));
-    // x.grad = Some(a_square.backward(&a.grad.unwrap()));
+    x.clone().backward();
+    a.clone().backward();
+    b.clone().backward();
+    y.clone().backward();
 
-    // println!("x.grad: {}", x.grad.unwrap());
+    // 逆伝播
+    // x.grad <- a.backward <- a.grad <- b.backward <- b.grad <- c.backward <- y.grad(=1.0)
+    y.grad = Some(Array::from_elem(IxDyn(&[]), 1.0));
+    println!("y.grad: {}", y.grad.clone().unwrap());
+    b.grad = Some(c_square.backward(&y.grad.unwrap()));
+    println!("b.grad: {}", b.grad.clone().unwrap());
+    a.grad = Some(b_exp.backward(&b.grad.unwrap()));
+    x.grad = Some(a_square.backward(&a.grad.unwrap()));
+
+    println!("x.grad: {}", x.grad.unwrap());
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    /// ゼロの二乗のテスト。
     #[test]
-    fn test_todo() {
-        todo!();
+    fn test_zero_square() {
+        let x = Variable::new(Array::from_elem(IxDyn(&[]), 0.0));
+        let f = Square {
+            input: None,
+            output: None,
+        };
+        let rc_f = Rc::new(RefCell::new(f.clone()));
+        let y = f.call(&x, rc_f);
+        let expected = Array::from_elem(IxDyn(&[]), 0.0);
+        assert_eq!(expected, y.data);
+    }
+
+    /// 1の二乗のテスト。
+    #[test]
+    fn test_one_square() {
+        let x = Variable::new(Array::from_elem(IxDyn(&[]), 1.0));
+        let f = Square {
+            input: None,
+            output: None,
+        };
+        let rc_f = Rc::new(RefCell::new(f.clone()));
+        let y = f.call(&x, rc_f);
+        let expected = Array::from_elem(IxDyn(&[]), 1.0);
+        assert_eq!(expected, y.data);
+    }
+
+    /// 10の二乗のテスト。
+    #[test]
+    fn test_ten_square() {
+        let x = Variable::new(Array::from_elem(IxDyn(&[]), 10.0));
+        let f = Square {
+            input: None,
+            output: None,
+        };
+        let rc_f = Rc::new(RefCell::new(f.clone()));
+        let y = f.call(&x, rc_f);
+        let expected = Array::from_elem(IxDyn(&[]), 100.0);
+        assert_eq!(expected, y.data);
+    }
+
+    /// 負の値の二乗のテスト。
+    #[test]
+    fn test_negative_square() {
+        let x = Variable::new(Array::from_elem(IxDyn(&[]), -5.0));
+        let f = Square {
+            input: None,
+            output: None,
+        };
+        let rc_f = Rc::new(RefCell::new(f.clone()));
+        let y = f.call(&x, rc_f);
+        let expected = Array::from_elem(IxDyn(&[]), 25.0);
+        assert_eq!(expected, y.data);
+    }
+
+    /// Exp 関数のテスト。
+    #[test]
+    fn test_exp_2() {
+        let x = Variable::new(Array::from_elem(IxDyn(&[]), 2.0));
+        let f = Exp {
+            input: None,
+            output: None,
+        };
+        let rc_f = Rc::new(RefCell::new(f.clone()));
+        let y = f.call(&x, rc_f);
+        let e = std::f64::consts::E;
+        let expected = Array::from_elem(IxDyn(&[]), e.powf(2.0));
+
+        assert_eq!(expected, y.data);
     }
 }
---
+++ src/bin/step7_work.rs
@@ -0,0 +1,195 @@
+use ndarray::{Array, IxDyn};
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+/// Variable 構造体
+/// * data (Array<f64, IxDyn>): 変数
+/// * grad (Option<Array<f64, IxDyn>): 変数に対応した微分した値。逆伝播によって実際に微分が計算されたときに値を設定する。
+/// * creator (Option<Weak<RefCell<dyn Function>>>): 生成元の関数
+#[derive(Debug, Clone)]
+struct Variable {
+    data: Array<f64, IxDyn>,
+    grad: Option<Array<f64, IxDyn>>,
+    creator: Option<Weak<RefCell<dyn Function>>>,
+}
+
+impl Variable {
+    /// Variable のコンストラクタ。
+    ///
+    /// # Arguments
+    /// * data - 変数
+    fn new(data: Array<f64, IxDyn>) -> Variable {
+        Variable {
+            data,
+            grad: None,
+            creator: None,
+        }
+    }
+
+    /// この変数を生成した関数を設定する。
+    ///
+    /// # Arguments
+    /// * creator - 生成元の関数
+    fn set_creator(&mut self, creator: Rc<RefCell<dyn Function>>) {
+        self.creator = Some(Rc::downgrade(&creator));
+    }
+}
+
+/// Function トレイト
+/// Variable を入力し、処理を実行して結果を Variable で返却する。
+trait Function {
+    /// 入力値、出力値を設定する。
+    ///
+    /// Arguments
+    ///  * input (Rc<RefCell<Variable>>): 入力値
+    /// * output (Rc<RefCell<Variable>>): 出力値
+    fn set_parameters(&mut self, input: Rc<RefCell<Variable>>, output: Rc<RefCell<Variable>>);
+
+    /// 入力値、出力値を取得する。
+    /// 入力値、出力値が存在しない場合は None を返却する。
+    ///
+    /// Returns
+    /// * Option<Parameters>: 入力値、出力値
+    fn get_parameters(&self) -> Option<&Parameters>;
+
+    /// 順伝播
+    /// 通常の計算を行う順伝播。継承して実装すること。
+    ///
+    /// Arguments
+    /// * x (Array<f64, IxDyn>): 入力値
+    ///
+    /// Returns
+    /// * Array<f64, IxDyn>: 出力値
+    fn forward(&self, x: &Array<f64, IxDyn>) -> Array<f64, IxDyn>;
+
+    /// デバッグ用の出力
+    fn print(&self) {
+        println!(
+            "Function::print \n input: {:?} \n output: {:?}",
+            &self.get_parameters().unwrap().get_input().clone(),
+            &self.get_parameters().unwrap().get_output().clone(),
+        );
+    }
+}
+
+/// Parameters 構造体
+/// 関数に対する入力値、出力値を保持する。
+///
+/// Arguments
+/// * input (Option<Weak<RefCell<Variable>>>): 入力値
+/// * output (Option<Weak<RefCell<Variable>>>): 出力値
+#[derive(Debug, Clone)]
+struct Parameters {
+    input: Option<Weak<RefCell<Variable>>>,
+    output: Option<Weak<RefCell<Variable>>>,
+}
+impl Parameters {
+    /// コンストラクタ
+    /// 入力値、出力値を設定する。
+    ///
+    /// Arguments
+    /// * input (Rc<RefCell<Variable>>): 入力値
+    /// * output (Rc<RefCell<Variable>>): 出力値
+    fn new(input: Rc<RefCell<Variable>>, output: Rc<RefCell<Variable>>) -> Parameters {
+        Parameters {
+            input: Some(Rc::downgrade(&input)),
+            output: Some(Rc::downgrade(&output)),
+        }
+    }
+
+    /// 入力値を取得する。
+    /// 入力値が存在しない場合は None を返却する。
+    ///
+    /// Returns
+    /// * Option<Variable>: 入力値
+    fn get_input(&self) -> Option<Variable> {
+        println!("call get_input");
+
+        match &self.input {
+            Some(input) => {
+                if let Some(strong_input) = input.upgrade() {
+                    println!("get_input input data: {:?}", strong_input.borrow().data);
+                } else {
+                    println!("get_input unknow input.");
+                }
+                let result = input.upgrade().unwrap().borrow().clone();
+                Some(result)
+            }
+            None => {
+                println!("self.input is None.");
+                None
+            }
+        }
+    }
+
+    /// 出力値を取得する。
+    /// 出力値が存在しない場合は None を返却する。
+    ///
+    /// Returns
+    /// * Option<Variable>: 出力値
+    fn get_output(&self) -> Option<Variable> {
+        println!("call get_output");
+        match &self.output {
+            Some(output) => {
+                if let Some(strong_output) = output.upgrade() {
+                    // println!("get_output output data: {:?}", strong_output.borrow().data);
+                    Some(strong_output.borrow().clone())
+                } else {
+                    println!("get_output unknow output.");
+                    None
+                }
+            }
+            None => {
+                println!("self.output is None.");
+                None
+            }
+        }
+    }
+}
+
+/// 二乗する。
+#[derive(Debug, Clone)]
+struct Square {
+    parameters: Option<Parameters>,
+}
+
+impl Function for Square {
+    fn set_parameters(&mut self, input: Rc<RefCell<Variable>>, output: Rc<RefCell<Variable>>) {
+        self.parameters = Some(Parameters::new(input.clone(), output.clone()));
+    }
+    fn get_parameters(&self) -> Option<&Parameters> {
+        match &self.parameters {
+            Some(parameters) => Some(&parameters),
+            None => None,
+        }
+    }
+
+    /// 順伝播
+    fn forward(&self, x: &Array<f64, IxDyn>) -> Array<f64, IxDyn> {
+        let result = x.mapv(|x| x.powi(2));
+        dbg!(&result);
+        result
+    }
+}
+
+fn main() {
+    let mut square = Square { parameters: None };
+
+    let mut x = Rc::new(RefCell::new(Variable::new(Array::from_elem(
+        IxDyn(&[]),
+        0.5,
+    ))));
+
+    let mut y = Rc::new(RefCell::new(Variable::new(
+        square.forward(&x.borrow().data.clone()),
+    )));
+
+    dbg!(&x);
+    dbg!(&y);
+    square.set_parameters(x.clone(), y.clone());
+
+    let rc_square = Rc::new(RefCell::new(square.clone()));
+    x.borrow_mut().set_creator(rc_square);
+
+    square.print();
+}
