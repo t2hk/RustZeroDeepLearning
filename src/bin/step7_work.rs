4968f35f2cc3b55d466455917adf0120c80f518a
t2hk <30774256+t2hk@users.noreply.github.com>
2025-02-23 15:09:50 +0000

    ステップ7に向けてステップ6までのリファクタリング

--- src/bin/step7_work.rs
+++ src/bin/step7_work.rs
@@ -41,7 +41,7 @@ trait Function {
     /// 入力値、出力値を設定する。
     ///
     /// Arguments
-    ///  * input (Rc<RefCell<Variable>>): 入力値
+    /// * input (Rc<RefCell<Variable>>): 入力値
     /// * output (Rc<RefCell<Variable>>): 出力値
     fn set_parameters(&mut self, input: Rc<RefCell<Variable>>, output: Rc<RefCell<Variable>>);
 
@@ -49,7 +49,7 @@ trait Function {
     /// 入力値、出力値が存在しない場合は None を返却する。
     ///
     /// Returns
-    /// * Option<Parameters>: 入力値、出力値
+    /// * Option<&FunctionParameters>: 入力値、出力値
     fn get_parameters(&self) -> Option<&FunctionParameters>;
 
     /// 順伝播
@@ -62,26 +62,61 @@ trait Function {
     /// * Array<f64, IxDyn>: 出力値
     fn forward(&self, x: &Array<f64, IxDyn>) -> Array<f64, IxDyn>;
 
+    /// 微分の計算を行う逆伝播。
+    /// 継承して実装すること。
+    ///
+    /// Arguments
+    /// * gy (Array<f64, IxDyn>): 出力値に対する微分値
+    ///
+    /// Returns
+    /// * Array<f64, IxDyn>: 入力値に対する微分値
+    fn backward(&self, gy: &Array<f64, IxDyn>) -> Array<f64, IxDyn>;
+
+    /// 関数を実行する。
+    /// 関数の実装は Function を継承して行う。
+    ///
+    /// # Arguments
+    /// * input (Variable) - 変数
+    ///
+    /// # Return
+    /// * Variable - 処理結果
+    fn call(
+        &mut self,
+        input: &Variable,
+        self_rc: Rc<RefCell<dyn Function>>,
+    ) -> Rc<RefCell<Variable>> {
+        // 関数を実行する。
+        let x = Rc::new(RefCell::new(input.clone()));
+        let y = Rc::new(RefCell::new(Variable::new(self.forward(&x.borrow().data))));
+
+        // 関数の出力結果に生成元の関数を設定する。
+        y.borrow_mut().set_creator(Rc::clone(&self_rc));
+        // 関数の入力、出力を設定する。
+        self.set_parameters(x.clone(), y.clone());
+
+        y.clone()
+    }
+
     /// デバッグ用の出力
     fn print(&self) {
         println!(
             "Function::print \n input: {:?} \n output: {:?}",
-            &self.get_parameters().unwrap().get_input().clone(),
-            &self.get_parameters().unwrap().get_output().clone(),
+            self.get_parameters().unwrap().get_input().clone(),
+            self.get_parameters().unwrap().get_output().clone(),
         );
     }
 }
 
-/// Parameters 構造体
+/// FunctionParameters 構造体
 /// 関数に対する入力値、出力値を保持する。
 ///
 /// Arguments
-/// * input (Option<Weak<RefCell<Variable>>>): 入力値
-/// * output (Option<Weak<RefCell<Variable>>>): 出力値
+/// * input (Option<Rc<RefCell<Variable>>>): 入力値
+/// * output (Option<Rc<RefCell<Variable>>>): 出力値
 #[derive(Debug, Clone)]
 struct FunctionParameters {
-    input: Option<Weak<RefCell<Variable>>>,
-    output: Option<Weak<RefCell<Variable>>>,
+    input: Option<Rc<RefCell<Variable>>>,
+    output: Option<Rc<RefCell<Variable>>>,
 }
 impl FunctionParameters {
     /// コンストラクタ
@@ -92,8 +127,8 @@ impl FunctionParameters {
     /// * output (Rc<RefCell<Variable>>): 出力値
     fn new(input: Rc<RefCell<Variable>>, output: Rc<RefCell<Variable>>) -> FunctionParameters {
         FunctionParameters {
-            input: Some(Rc::downgrade(&input)),
-            output: Some(Rc::downgrade(&output)),
+            input: Some(input.clone()),
+            output: Some(output.clone()),
         }
     }
 
@@ -101,20 +136,12 @@ impl FunctionParameters {
     /// 入力値が存在しない場合は None を返却する。
     ///
     /// Returns
-    /// * Option<Variable>: 入力値
-    fn get_input(&self) -> Option<Variable> {
+    /// * Option<Rc<RefCell<Variable>>>: 入力値
+    fn get_input(&self) -> Option<Rc<RefCell<Variable>>> {
         println!("call get_input");
 
         match &self.input {
-            Some(input) => {
-                if let Some(strong_input) = input.upgrade() {
-                    println!("get_input input data: {:?}", strong_input.borrow().data);
-                } else {
-                    println!("get_input unknow input.");
-                }
-                let result = input.upgrade().unwrap().borrow().clone();
-                Some(result)
-            }
+            Some(input) => Some(input.clone()),
             None => {
                 println!("self.input is None.");
                 None
@@ -126,21 +153,13 @@ impl FunctionParameters {
     /// 出力値が存在しない場合は None を返却する。
     ///
     /// Returns
-    /// * Option<Variable>: 出力値
-    fn get_output(&self) -> Option<Variable> {
+    /// * Option<Rc<RefCell<Variable>>>: 出力値
+    fn get_output(&self) -> Option<Rc<RefCell<Variable>>> {
         println!("call get_output");
         match &self.output {
-            Some(output) => {
-                if let Some(strong_output) = output.upgrade() {
-                    // println!("get_output output data: {:?}", strong_output.borrow().data);
-                    Some(strong_output.borrow().clone())
-                } else {
-                    println!("get_output unknow output.");
-                    None
-                }
-            }
+            Some(output) => Some(output.clone()),
             None => {
-                println!("self.output is None.");
+                println!("self.input is None.");
                 None
             }
         }
@@ -154,9 +173,11 @@ struct Square {
 }
 
 impl Function for Square {
+    // 関数の入出力値のセッター
     fn set_parameters(&mut self, input: Rc<RefCell<Variable>>, output: Rc<RefCell<Variable>>) {
         self.parameters = Some(FunctionParameters::new(input.clone(), output.clone()));
     }
+    // 関数の入出力値のゲッター
     fn get_parameters(&self) -> Option<&FunctionParameters> {
         match &self.parameters {
             Some(parameters) => Some(&parameters),
@@ -170,26 +191,87 @@ impl Function for Square {
         dbg!(&result);
         result
     }
+
+    /// 逆伝播
+    /// y=x^2 の微分であるため、dy/dx=2x である。
+    fn backward(&self, gy: &Array<f64, IxDyn>) -> Array<f64, IxDyn> {
+        let x = self
+            .get_parameters()
+            .unwrap()
+            .get_input()
+            .as_ref()
+            .unwrap()
+            .borrow()
+            .data
+            .clone();
+        let gx = 2.0 * x * gy;
+        gx
+    }
+}
+
+/// Exp 関数
+#[derive(Debug, Clone)]
+struct Exp {
+    parameters: Option<FunctionParameters>,
+}
+impl Function for Exp {
+    // 関数の入出力値のセッター
+    fn set_parameters(&mut self, input: Rc<RefCell<Variable>>, output: Rc<RefCell<Variable>>) {
+        self.parameters = Some(FunctionParameters::new(input.clone(), output.clone()));
+    }
+    // 関数の入出力値のゲッター
+    fn get_parameters(&self) -> Option<&FunctionParameters> {
+        match &self.parameters {
+            Some(parameters) => Some(&parameters),
+            None => None,
+        }
+    }
+    // Exp (y=e^x) の順伝播
+    fn forward(&self, x: &Array<f64, IxDyn>) -> Array<f64, IxDyn> {
+        let e = std::f64::consts::E;
+        x.mapv(|x| e.powf(x))
+    }
+
+    /// 逆伝播
+    /// dy/dx=e^x である。
+    fn backward(&self, gy: &Array<f64, IxDyn>) -> Array<f64, IxDyn> {
+        let x = &self
+            .get_parameters()
+            .unwrap()
+            .get_input()
+            .as_ref()
+            .unwrap()
+            .borrow()
+            .data
+            .clone();
+        let e = std::f64::consts::E;
+        let gx = x.mapv(|x| e.powf(x)) * gy;
+        gx
+    }
 }
 
 fn main() {
-    let mut square = Square { parameters: None };
+    let mut a_square = Square { parameters: None };
+    let mut b_exp = Exp { parameters: None };
+    let mut c_square = Square { parameters: None };
 
-    let mut x = Rc::new(RefCell::new(Variable::new(Array::from_elem(
+    let x = Rc::new(RefCell::new(Variable::new(Array::from_elem(
         IxDyn(&[]),
         0.5,
     ))));
 
-    let mut y = Rc::new(RefCell::new(Variable::new(
-        square.forward(&x.borrow().data.clone()),
-    )));
+    let a = a_square.call(&x.borrow(), Rc::new(RefCell::new(a_square.clone())));
+    let b = b_exp.call(&a.borrow(), Rc::new(RefCell::new(b_exp.clone())));
+    let y = c_square.call(&b.borrow(), Rc::new(RefCell::new(c_square.clone())));
 
-    dbg!(&x);
+    y.borrow_mut().grad = Some(Array::from_elem(IxDyn(&[]), 1.0));
+    b.borrow_mut().grad = Some(c_square.backward(y.borrow().grad.as_ref().unwrap()));
+    a.borrow_mut().grad = Some(b_exp.backward(b.borrow().grad.as_ref().unwrap()));
+    x.borrow_mut().grad = Some(a_square.backward(a.borrow().grad.as_ref().unwrap()));
+
     dbg!(&y);
-    square.set_parameters(x.clone(), y.clone());
+    dbg!(&x.borrow().grad);
 
-    let rc_square = Rc::new(RefCell::new(square.clone()));
-    x.borrow_mut().set_creator(rc_square);
-
-    square.print();
+    println!("y: {:?}, x.grad: {:?}", y.borrow().data, x.borrow().grad);
+    c_square.print();
 }
