f58b0da1236891a3e31690880c20d19fd6470de4
t2hk <30774256+t2hk@users.noreply.github.com>
2025-03-12 20:06:22 +0000

    Variableの作り方を見直し中

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,9 +1,12 @@
 use core::fmt::Debug;
 use ndarray::{Array, Dimension, IxDyn, ShapeBuilder};
+use num::Num;
+use rand::TryRngCore;
 use std::cell::RefCell;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::error::Error;
+use std::ops::Deref;
 use std::rc::{Rc, Weak};
 
 #[derive(Debug, Clone)]
@@ -15,53 +18,78 @@ struct Variable<A, D: Dimension> {
     generation: i32,
 }
 
-impl<A, D: Dimension> Variable<A, D> {
-    /// Variable のコンストラクタ。
-    ///
-    /// # Arguments
-    /// * data - 変数    
-    fn new<T: CreateVariable<A, D>>(data: T) -> Variable<A, D> {
-        CreateVariable::create_variable(&data)
-    }
+trait CreateVariable<D: Dimension> {
+    fn create_variable<Sh>(&self, shape: Sh) -> Result<Variable<Self, D>, Box<dyn Error>>
+    where
+        Self: Num + Clone + Sized,
+        Sh: ShapeBuilder<Dim = D>;
+}
 
-    /// ベクトルから配列を生成して構造体を構築
-    fn from_shape_vec<Sh>(
-        shape: Sh,
-        data: Vec<A>,
-        metadata: Option<String>,
-    ) -> Result<Self, Box<dyn Error>>
+// 全数値型に対する実装
+impl<A, D> CreateVariable<D> for A
+where
+    A: Num + Clone + 'static,
+    D: Dimension,
+{
+    fn create_variable<Sh>(&self, shape: Sh) -> Result<Variable<A, D>, Box<dyn Error>>
     where
         Sh: ShapeBuilder<Dim = D>,
     {
-        let arr = Array::from_shape_vec(shape, data)?;
-        Ok(Self {
-            data: arr,
+        Ok(Variable {
+            data: Array::from_shape_vec(shape, vec![self.clone(); shape.size()])?,
             name: None,
             grad: None,
-            // creator: None,
             generation: 0,
         })
     }
 }
 
-/// Variable 構造体を生成するためのトレイト
-/// * create_variable: Variable 構造体を生成する
-trait CreateVariable<A, D: Dimension> {
-    fn create_variable(&self) -> Variable<A, D>;
+impl<D: Dimension> CreateVariable<D> for Vec<f64> {
+    fn create_variable<Sh>(&self, shape: Sh) -> Result<Variable<f64, D>, Box<dyn Error>>
+    where
+        Sh: ShapeBuilder<Dim = D>,
+    {
+        Ok(Variable {
+            data: Array::from_shape_vec(shape, self.clone())?,
+            name: None,
+            grad: None,
+            generation: 0,
+        })
+    }
+}
+
+fn type_of<T>(_: T) -> String {
+    let a = std::any::type_name::<T>();
+    return a.to_string();
 }
 
 fn main() {
     let vector_data = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
-    let matrix = Variable::<f64, ndarray::Ix2>::from_shape_vec(
-        (2, 3), // 2x3の2次元配列
-        vector_data,
-        Some("サンプルデータ".to_string()),
-    )
-    .unwrap();
-    dbg!(&matrix);
+    // let matrix = Variable::<f64, ndarray::Ix2>::from_shape_vec(
+    //     (2, 3), // 2x3の2次元配列
+    //     vector_data,
+    //     Some("サンプルデータ".to_string()),
+    // )
+    // .unwrap();
+    // dbg!(&matrix);
 
-    let arr = Array::from_elem(IxDyn(&[]), 2.0);
-    let var = Variable::<i32, IxDyn>::from_shape_vec(IxDyn(&[]), vec![2], Some("hoge".to_string()))
-        .unwrap();
-    dbg!(&var);
+    //    let arr = Array::from_elem(IxDyn(&[]), 2.0);
+    // //let var = Variable::<i32, IxDyn>::from_shape_vec(IxDyn(&[]), vec![2], Some("hoge".to_string()))
+    //     .unwrap();
+    // dbg!(&var);
+
+    // let arr_val = Variable::new(&arr);
+
+    // println!("{}", type_of(&arr));
+    // println!("{}", type_of(&var));
+
+    let vector1 = 5.0.create_variable((3,)); // 3要素のベクトル
+    let vector2 = 3.create_variable((3,)); // 3要素のベクトル
+    let matrix = 1.0.create_variable((2, 2)); // 2x2行列
+    let matrix2 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0].create_variable((2, 2, 2));
+
+    dbg!(&vector1);
+    dbg!(&vector2);
+    dbg!(&matrix);
+    dbg!(&matrix2);
 }
