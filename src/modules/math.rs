13438652229dc1329bf068103daa95229d3f5eb2
t2hk <30774256+t2hk@users.noreply.github.com>
2025-03-19 21:26:02 +0000

    演算オーバーロード対応に向けて計算用の構造体の名称を変更

--- src/modules/math.rs
+++ src/modules/math.rs
@@ -4,12 +4,13 @@ use crate::modules::variable::*;
 use core::fmt::Debug;
 use ndarray::{Array, IxDyn};
 use std::cell::RefCell;
+use std::ops::{Add, Mul};
 use std::rc::Rc;
 
 /// 加算関数
 #[derive(Debug, Clone)]
-pub struct Add;
-impl<V: MathOps> Function<V> for Add {
+pub struct AddFunction;
+impl<V: MathOps> Function<V> for AddFunction {
     // Add (加算) の順伝播
     fn forward(&self, xs: Vec<Array<V, IxDyn>>) -> Vec<Array<V, IxDyn>> {
         let result = vec![&xs[0] + &xs[1]];
@@ -39,7 +40,7 @@ pub fn add<V: MathOps>(
     x1: Rc<RefCell<Variable<V>>>,
     x2: Rc<RefCell<Variable<V>>>,
 ) -> Rc<RefCell<Variable<V>>> {
-    let mut add = FunctionExecutor::new(Rc::new(RefCell::new(Add)));
+    let mut add = FunctionExecutor::new(Rc::new(RefCell::new(AddFunction)));
     // 加算の順伝播
     add.forward(vec![x1.clone(), x2.clone()])
         .get(0)
@@ -49,8 +50,8 @@ pub fn add<V: MathOps>(
 
 /// 乗算関数
 #[derive(Debug, Clone)]
-pub struct Mul;
-impl<V: MathOps> Function<V> for Mul {
+pub struct MulFunction;
+impl<V: MathOps> Function<V> for MulFunction {
     // Mul (乗算) の順伝播
     fn forward(&self, xs: Vec<Array<V, IxDyn>>) -> Vec<Array<V, IxDyn>> {
         let result = vec![&xs[0] * &xs[1]];
@@ -86,7 +87,7 @@ pub fn mul<V: MathOps>(
     x1: Rc<RefCell<Variable<V>>>,
     x2: Rc<RefCell<Variable<V>>>,
 ) -> Rc<RefCell<Variable<V>>> {
-    let mut mul = FunctionExecutor::new(Rc::new(RefCell::new(Mul)));
+    let mut mul = FunctionExecutor::new(Rc::new(RefCell::new(MulFunction)));
     // 乗算の順伝播
     mul.forward(vec![x1.clone(), x2.clone()])
         .get(0)
@@ -96,14 +97,11 @@ pub fn mul<V: MathOps>(
 
 /// 二乗関数
 #[derive(Debug, Clone)]
-pub struct Square;
-impl<V: MathOps> Function<V> for Square {
+pub struct SquareFunction;
+impl<V: MathOps> Function<V> for SquareFunction {
     /// 順伝播
     fn forward(&self, xs: Vec<Array<V, IxDyn>>) -> Vec<Array<V, IxDyn>> {
-        //let result = vec![xs[0].mapv(|x| x.pow(V::from(2).unwrap()))];
         let result = vec![xs[0].mapv(|x| x * x)];
-
-        //dpg!(result);
 
         result
     }
@@ -116,7 +114,6 @@ impl<V: MathOps> Function<V> for Square {
         gys: Vec<Array<V, IxDyn>>,
     ) -> Vec<Array<V, IxDyn>> {
         let x = inputs[0].borrow().get_data();
-        // let gxs = vec![V::from(2).unwrap() * &x * &gys[0].clone()];
         let x_gys = &gys[0].clone() * &x;
         let gxs = vec![x_gys.mapv(|x| x * V::from(2).unwrap())];
         gxs
@@ -131,21 +128,18 @@ impl<V: MathOps> Function<V> for Square {
 /// Return
 /// * Rc<RefCell<Variable>>: 二乗の結果
 pub fn square<V: MathOps>(input: Rc<RefCell<Variable<V>>>) -> Rc<RefCell<Variable<V>>> {
-    let mut square = FunctionExecutor::new(Rc::new(RefCell::new(Square)));
+    let mut square = FunctionExecutor::new(Rc::new(RefCell::new(SquareFunction)));
     // 二乗の順伝播
     square.forward(vec![input]).get(0).unwrap().clone()
 }
 
 /// Exp 関数
 #[derive(Debug, Clone)]
-pub struct Exp;
-impl<V: MathOps> Function<V> for Exp {
+pub struct ExpFunction;
+impl<V: MathOps> Function<V> for ExpFunction {
     // Exp (y=e^x) の順伝播
     fn forward(&self, xs: Vec<Array<V, IxDyn>>) -> Vec<Array<V, IxDyn>> {
         let e = std::f64::consts::E;
-        // let result = vec![xs[0].mapv(|x| e.powf(x.to_f64().unwrap()))];
-
-        // let result = vec![xs[0].mapv(|x| V::from(x.to_f64().unwrap().exp()).unwrap())];
         let result = vec![xs[0].mapv(|x| V::from(e.powf(x.to_f64().unwrap())).unwrap())];
 
         result
@@ -159,7 +153,7 @@ impl<V: MathOps> Function<V> for Exp {
         gys: Vec<Array<V, IxDyn>>,
     ) -> Vec<Array<V, IxDyn>> {
         let e = std::f64::consts::E;
-        let x = inputs[0].borrow().get_data().clone();
+        let x = inputs[0].borrow().get_data();
         let gys_val = gys[0].clone();
         let x_exp = vec![x.mapv(|x| V::from(e.powf(x.to_f64().unwrap())).unwrap())];
         let gxs = x_exp.iter().map(|x_exp| x_exp * &gys_val).collect();
@@ -175,7 +169,7 @@ impl<V: MathOps> Function<V> for Exp {
 /// Return
 /// * Rc<RefCell<Variable>>: 結果
 pub fn exp<V: MathOps>(input: Rc<RefCell<Variable<V>>>) -> Rc<RefCell<Variable<V>>> {
-    let mut exp = FunctionExecutor::new(Rc::new(RefCell::new(Exp)));
+    let mut exp = FunctionExecutor::new(Rc::new(RefCell::new(ExpFunction)));
     // EXP の順伝播
     exp.forward(vec![input.clone()]).get(0).unwrap().clone()
 }
@@ -216,8 +210,6 @@ mod tests {
 
         // 2乗した結果の期待値を計算する。
         let expected_output_data = Array::from_elem(IxDyn(&[]), rand_x * rand_x);
-        let expected_grad_val = rand_x * 2.0 * 1.0;
-        let expected_output_grad = Array::from_elem(IxDyn(&[]), expected_grad_val);
 
         // 順伝播実行する。
         let result = square(Rc::clone(&x));
@@ -233,7 +225,6 @@ mod tests {
 
         let e = std::f64::consts::E;
         let expected_output_data = Array::from_elem(IxDyn(&[]), e.powf(2.0));
-        dbg!(expected_output_data.clone());
 
         // 順伝播、逆伝播を実行する。
         let result = exp(Rc::clone(&x));
