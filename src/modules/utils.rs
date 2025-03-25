53c23f97371697e0084d820120080218fa6f1daa
t2hk <tthk.inoue@gmail.com>
2025-03-25 15:19:47 +0900

    ステップ26　dot_varを追加

--- src/modules/utils.rs
+++ src/modules/utils.rs
@@ -7,8 +7,20 @@ use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::rc::{Rc, Weak};
 
-// fn dot_var()
+/// Variable を graphviz の DOT 言語で出力する。
+///
+/// Arguments:
+/// * variable (Variable<V>): 出力対象の変数
+/// * verbose (bool): 詳細を出力するかどうか(省略可能)
+///
+/// Returns:
+/// * String: 変数の DOT 言語表記
 macro_rules! dot_var {
+    // verbose を指定しない場合は false で実行する。
+    ($variable:ident) => {{
+        dot_var!($variable, false)
+    }};
+    // verbose の指定がある場合の実行
     ($variable:ident, $verbose:ident) => {{
         let mut v_name = "".to_string();
         let temp_raw_v = $variable.raw().borrow().clone();
@@ -34,6 +46,8 @@ macro_rules! dot_var {
 mod test {
     use super::*;
 
+    /// dot_var のテスト
+    /// スカラ値、詳細情報を出力
     #[test]
     fn test_dot_var_1() {
         let var = Variable::new(RawVariable::new(2.0));
@@ -42,6 +56,8 @@ mod test {
         println!("{}", result);
     }
 
+    /// dot_var のテスト
+    /// 行列、詳細情報を出力
     #[test]
     fn test_dot_var_2() {
         let var = Variable::new(RawVariable::from_shape_vec(
@@ -50,6 +66,19 @@ mod test {
         ));
         var.borrow_mut().set_name("2x2dim".to_string());
         let result = dot_var!(var, true);
+        println!("{}", result);
+    }
+
+    /// dot_var のテスト
+    /// 行列、詳細情報なし
+    #[test]
+    fn test_dot_var_3() {
+        let var = Variable::new(RawVariable::from_shape_vec(
+            vec![2, 2],
+            vec![10, 20, 30, 40],
+        ));
+        var.borrow_mut().set_name("2x2dim".to_string());
+        let result = dot_var!(var);
         println!("{}", result);
     }
 }
