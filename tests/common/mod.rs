a78616e41a83bc3d5a75457d31eb2dab01645921
t2hk <30774256+t2hk@users.noreply.github.com>
2025-04-05 18:00:15 +0000

    テストモードの変更

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -7,7 +7,7 @@ static INIT: Once = Once::new();
 
 pub fn setup() {
     INIT.call_once(|| {
-        env::set_var("RUST_LOG", "debug");
+        env::set_var("RUST_LOG", "info");
 
         env_logger::init();
     });
