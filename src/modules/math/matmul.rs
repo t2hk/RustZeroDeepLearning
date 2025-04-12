f6bbffb310be2ba87e224e68752837d8aa899532
t2hk <30774256+t2hk@users.noreply.github.com>
2025-04-12 11:22:30 +0000

    微修正

--- src/modules/math/matmul.rs
+++ src/modules/math/matmul.rs
@@ -4,8 +4,8 @@ use crate::modules::math::*;
 use core::fmt::Debug;
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
-use ndarray::{linalg::Dot, Array1, Array2, ArrayD, Ix1, Ix2};
-use ndarray::{Array, IxDyn, LinalgScalar};
+use ndarray::{Array, IxDyn};
+use ndarray::{Ix1, Ix2};
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -194,8 +194,6 @@ pub fn matmul<V: MathOps>(x: Variable<V>, w: Variable<V>) -> Variable<V> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ndarray::{Ix1, Ix2, Ix3};
-    use rand::prelude::*;
 
     #[test]
     fn test_forward1() {
